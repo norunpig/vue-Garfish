303e8f6d54c4d9879cefada75b4c354d71aa701a
norunpig <852634825@qq.com>
2022-11-11 15:41:34 +0800

    1 binaries uploaded

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,11 @@ module.exports = {
       {
         test: /\.(png|jpg|gif|jpeg|svg)$/i,
         type: 'asset/resource',
-      }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ]
   }
 };
