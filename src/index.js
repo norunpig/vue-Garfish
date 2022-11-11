c917ef990b4ceee6049b6d1cd8e9f6d731ab3b79
norunpig <852634825@qq.com>
2022-11-11 15:35:17 +0800

    1 file modified

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ function component() {
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+  // 将图像添加到我们已经存在的 div 中。
+  const myIcon = new Image();
+  myIcon.src = Icon;
 
+  element.appendChild(myIcon);
   return element;
 }
 
